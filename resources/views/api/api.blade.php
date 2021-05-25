a84b14a9cc07413e58bef5c217af433c51de246f
Gaspare Joubert <gasparejoubert@gmail.com>
2021-05-25 15:45:12 +0100

    User edit view Route
    Add models, view and controller for the User edit view route.

--- resources/views/api/api.blade.php
+++ resources/views/api/api.blade.php
@@ -7,20 +7,20 @@
     <title>Q.A.N.W. | A.P.I.</title>
 
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="../../images/icons/favicon.ico"/>
+    <link rel="icon" type="image/png" href="{{asset('vendor/images/icons/favicon.ico')}}"/>
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/animate/animate.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/select2/select2.min.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../vendor/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/perfect-scrollbar/perfect-scrollbar.css')}}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../css/util.css">
-    <link rel="stylesheet" type="text/css" href="../../css/main.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/css/util.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/css/main.css')}}">
     <!--===============================================================================================-->
 </head>
 <body>
@@ -48,14 +48,14 @@
 
 
 <!--===============================================================================================-->
-<script src="../../vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="{{asset('vendor/jquery/jquery-3.2.1.min.js')}}"></script>
 <!--===============================================================================================-->
-<script src="../../vendor/bootstrap/js/popper.js"></script>
-<script src="../../vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{asset('vendor/bootstrap/js/popper.js')}}"></script>
+<script src="{{asset('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
 <!--===============================================================================================-->
-<script src="../../vendor/select2/select2.min.js"></script>
+<script src="{{asset('vendor/select2/select2.min.js')}}"></script>
 <!--===============================================================================================-->
-<script src="../../js/main.js"></script>
+<script src="{{asset('vendor/js/main.js')}}"></script>
 
 </body>
 </html>
--- resources/views/user/user_list_view.blade.php
+++ resources/views/user/user_list_view.blade.php
@@ -5,22 +5,23 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Q.A.N.W. | Users List View</title>
-<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="../../images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../../vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../../vendor/animate/animate.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../../vendor/select2/select2.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../../vendor/perfect-scrollbar/perfect-scrollbar.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../../css/util.css">
-	<link rel="stylesheet" type="text/css" href="../../css/main.css">
-<!--===============================================================================================-->
+
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{asset('vendor/images/icons/favicon.ico')}}"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/animate/animate.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/select2/select2.min.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/perfect-scrollbar/perfect-scrollbar.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/css/util.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('vendor/css/main.css')}}">
+    <!--===============================================================================================-->
 </head>
 <body>
 
@@ -67,14 +68,14 @@
 
 
     <!--===============================================================================================-->
-    <script src="../../vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script src="{{asset('vendor/jquery/jquery-3.2.1.min.js')}}"></script>
     <!--===============================================================================================-->
-    <script src="../../vendor/bootstrap/js/popper.js"></script>
-    <script src="../../vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="{{asset('vendor/bootstrap/js/popper.js')}}"></script>
+    <script src="{{asset('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
     <!--===============================================================================================-->
-    <script src="../../vendor/select2/select2.min.js"></script>
+    <script src="{{asset('vendor/select2/select2.min.js')}}"></script>
     <!--===============================================================================================-->
-    <script src="../../js/main.js"></script>
+    <script src="{{asset('vendor/js/main.js')}}"></script>
 
 </body>
 </html>
