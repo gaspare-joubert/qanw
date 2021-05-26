118424eef75b555388e7e12763e631c860c34360
Gaspare Joubert <gasparejoubert@gmail.com>
2021-05-26 22:24:05 +0100

    Album list view Route
    Add models, view and controller for the Albums list view route.
    
    Updated Paginator to use Bootstrap.
    Prevent pagination with unexpected styling.

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -10,9 +10,9 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redirect;
-
 use function React\Promise\all;
 
 /**
@@ -23,101 +23,25 @@ class AlbumsController extends Controller
 {
     /**
      * Get all albums and return list view.
-     *
-     * @return Application|Factory|View
      */
     public function albumsListView()
     {
-        $albumMetadata = new Album();
         $data = array();
 
         // compile the table headers
         $data['headers'][] = 'id';
-        $fillables = $albumMetadata->getFillable();
-        foreach ($fillables as $fillable) {
-            $data['headers'][] = $fillable;
+        $data['headers'][] = 'title';
+
+        $albums = DB::table('albums')
+            ->select('id', 'title')
+            ->paginate(15);
+
+        if ($albums) {
+            return view('album/album_list_view', ['data' => $data, 'albums' => $albums]);
         }
 
-        // compile the table rows
-        $albumItems = Album::all(); // returns $this->items
-        foreach ($albumItems as $albumItem) {
-            $data['rows'][] = $albumItem->getAttributes();
-        }
+        Log::error("Unable to retrieve albums.");
 
-        // todo: carry on from here
-        return view('album/album_list_view', ['data' => $data]);
-    }
-
-    /**
-     * Get the selected user data and return the user edit view.
-     *
-     * @param string $id
-     * @return Application|Factory|View|RedirectResponse
-     */
-    public function userEditView(string $id)
-    {
-        $user = User::whereId($id)->first();
-
-        if ($user) {
-            $attributes = $user->getAttributes();
-
-            if ($attributes) {
-                $name = $attributes['name'];
-                $email = $attributes['email'];
-
-                return view(
-                    'user/user_edit_view',
-                    [
-                        'userId' => $id,
-                        'userName' => $name,
-                        'email' => $email,
-                    ]
-                );
-            }
-
-            Log::error("Unable to retrieve user $id attributes.");
-
-            return Redirect::back()->with('status', 'Error. User details not found.');
-        }
-
-        Log::error("Unable to retrieve user $id details.");
-
-        return Redirect::back()->with('status', 'Error. User details not found.');
-    }
-
-    /**
-     * Validate request and store user details.
-     *
-     * @param StoreUserRequest $request
-     * @return RedirectResponse
-     */
-    public function userStore(StoreUserRequest $request)
-    {
-        if ($request->validated()) {
-            try {
-                $user = User::whereId($request->userId)->first();
-                $this->defineUserFieldsToUpdate($user, $request);
-                $user->save();
-
-                return redirect()->route('users_list_view')->with('success', 'User details have been updated.');
-            } catch (\Exception $ex) {
-                Log::error($ex->getMessage());
-
-                return redirect()->route('users_list_view')->with('status', 'Error. User details have not updated.');
-            }
-        }
-    }
-
-    /**
-     * Define the user fields to store or update.
-     *
-     * @param User $user
-     * @param StoreUserRequest $request
-     * @return void
-     */
-    private function defineUserFieldsToUpdate(User $user, StoreUserRequest $request): void
-    {
-        $user->name = $request->userName;
-        $user->email = $request->email;
+        return Redirect::back()->with('status', 'Error. Unable to retrieve albums.');
     }
 }
